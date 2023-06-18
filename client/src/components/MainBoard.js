30b7a304560f768e4b673b49695402688b42f712
kalle.kindvall <kalle.kindvall@dacsa.se>
2023-06-18 21:27:51 +0200

    Basci Structure done, Height issues

--- client/src/components/MainBoard.js
+++ client/src/components/MainBoard.js
@@ -1,46 +1,104 @@
 import React from 'react';
-import { Container, Grid, Paper, TextField, Button } from '@mui/material';
+import { Container, Grid, Paper, TextField, Button, Typography } from '@mui/material';
 import { Row } from '../base/Row'; // replace with your actual file path
 import { GridPaper } from '../base/GridPaper'; // replace with your actual file path
 
-export function Board2() {
+
+const classes = {
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: 20,
+    textAlign: "center",
+  }
+};
+//the InnerGrid component will be a collection of multiple Grid items
+function InnerGrid() {
   return (
-    <Container spacing={2}  style={{ height: '100vh' }}>
-      <Row height='10vh'>
-        <GridPaper width={3}>
-          <p style={{ margin: 0 }}>Game Info</p>
-
-        </GridPaper>
-        <GridPaper width={6} >
-          <TextField label="Input Field" />
-        </GridPaper>
-        <GridPaper width={3} >
-          <Button variant="contained">Button</Button>
-        </GridPaper>
-      </Row>
-
-      <Row height='60vh'>
-      <GridPaper width={4}>
-          <p >Drawable Game Cards</p>
-        </GridPaper>
-        <GridPaper width={8}>
-          <p  style={{ margin: 0 }}>Main Game Board</p>
-        </GridPaper>
-        
-      </Row>
-
-      <Row height='25vh'>
-        
-        <GridPaper width={2}>
-          <Button variant="contained">Button</Button>
-        </GridPaper>
-        <GridPaper width={8}>
-          <p style={{ margin: 0 }}>Player's Hand</p>
-        </GridPaper>
-        <GridPaper width={2}>
-          <Button variant="contained">Button</Button>
-        </GridPaper>
-      </Row>
-    </Container>
+    <>
+      <Grid item xs={4}>
+        <Paper style={classes.paper}>item</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper style={classes.paper}>item</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper style={classes.paper}>item</Paper>
+      </Grid>
+    </>
   );
 }
+
+const RowContainer = ({ xs, children }) => (
+  <Grid item xs={xs} sx={{ height: "100%", width: "100vw" }}>
+    <Grid container alignItems={"center"} spacing={1} direction={"row"} sx={{ height: "100%" }}>
+      {children}
+    </Grid>
+  </Grid>
+);
+
+
+const GridItem = ({ xs, children }) => (
+  <Grid item xs={xs}>
+    <Paper style={classes.paper}>
+      {children}
+    </Paper>
+
+  </Grid>
+);
+export const Board2 = () => {
+  return (
+    <Grid container spacing={1} height={"100vh"} direction={"column"}>
+      <RowContainer xs={1}>
+        <GridItem xs={3}>
+          <Typography display="inline" >
+            ROOM CODE:
+          </Typography>
+          <Typography display="inline" color="text.secondary">
+            2754
+          </Typography>
+
+        </GridItem>
+        <GridItem xs={6}>
+          <Button>Start Game</Button>
+        </GridItem>
+        <GridItem xs={3}>
+          <Typography display="inline" >
+            Opponent:
+          </Typography>
+          <Typography display="inline" color="text.secondary">
+            AI(Until Human Join)
+          </Typography>
+
+        </GridItem>
+      </RowContainer>
+      <RowContainer xs={7}>
+        <GridItem xs={3}>Opponent nr of cards on hand <br></br> Played Cards </GridItem>
+        <GridItem xs={7}>COMMON CARDS<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br></GridItem>
+        <GridItem xs={2}>
+          <Grid container alignItems={"center"} spacing={1} direction={"column"} >
+            <Grid item xs={6}>
+              <Paper style={classes.paper}>
+                Development Cards
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper style={classes.paper}>
+                  Risk <br></br> Cards
+              </Paper>
+            </Grid>
+
+
+          </Grid>
+
+        </GridItem>
+      </RowContainer>
+      <RowContainer xs={4}>
+        <GridItem xs={4}>Your Played Cards</GridItem>
+        <GridItem xs={6}>CARDS ON HAND HERE</GridItem>
+        <GridItem xs={2}><Button>Lock In</Button></GridItem>
+      </RowContainer>
+    </Grid>
+  );
+};
