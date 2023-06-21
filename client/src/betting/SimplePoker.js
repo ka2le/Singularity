8af9693cb610e0b38e5e0ada8dfe757873c0b1ee
kalle.kindvall <kalle.kindvall@dacsa.se>
2023-06-21 11:01:58 +0200

    2 rounds working

--- client/src/betting/SimplePoker.js
+++ client/src/betting/SimplePoker.js
@@ -52,7 +52,7 @@ export const SimplePokerContainer = () => {
     }
 
     async function trainWithModelAiVsAi(numGames, numEpochs, model = null) {
-        const gameHistories = await runGamesWithModel(numGames, "ai","ai");
+        const gameHistories = await runGamesWithModel(numGames, "ai", "ai");
         const trainedModel = await trainModel(gameHistories, numEpochs, model);
         setModel(trainedModel);
         console.log("MODEL TRAINED");
@@ -73,16 +73,16 @@ export const SimplePokerContainer = () => {
     return (
         <div>
             <h1>SIMPLE POKER GAME</h1>
-            <button onClick={() => train(3000, 40)}>Train New Model - Random Vs Random</button>
-            <button onClick={() => trainFixed(300, 40)}>Train New Model - Fixed Vs Random</button><br></br><br></br>
-            <button onClick={() => train(200, 50, model)}>Continue Training Model - Random Vs Random</button>
-            <button onClick={() => trainWithModel(200, 50, model)}>Continue Training Model - Ai vs Random</button>
-            <button onClick={() => trainWithModelAiVsAi(50, 50, model)}>Continue Training Model - Ai vs Ai</button><br></br><br></br>
+            <button onClick={() => train(200, 100)}>Train New Model - Random Vs Random</button>
+            <button onClick={() => trainFixed(300, 100)}>Train New Model - Fixed Vs Random</button><br></br><br></br>
+            <button onClick={() => train(500, 100, model)}>Continue Training Model - Random Vs Random</button>
+            <button onClick={() => trainWithModel(500, 100, model)}>Continue Training Model - Ai vs Random</button>
+            <button onClick={() => trainWithModelAiVsAi(50, 100, model)}>Continue Training Model - Ai vs Ai</button><br></br><br></br>
             <button onClick={saveModel}>Save Model</button>
             <button onClick={loadModel}>Load Model</button><br></br><br></br>
             <button onClick={() => runRandomGames(1000)}>Run Random Games</button>
-            <button onClick={() => runGamesWithModel(5, "ai")}>Run Games</button> 
-            <button onClick={() => runGamesWithModel(100, "ai", "ai")}>Run Games Ai vs Ai</button> 
+            <button onClick={() => runGamesWithModel(5, "ai")}>Run Games</button>
+            <button onClick={() => runGamesWithModel(100, "ai", "ai")}>Run Games Ai vs Ai</button>
         </div>
     );
 }
@@ -97,10 +97,11 @@ const preprocess = (gameStates) => {
         let input = [
             gameState.p1Hand,
             gameState.p1Bet,
-            //gameState.p1Money,
+            gameState.p1Money,
+            gameState.round
         ]
         inputData.push(input);
-        let output = gameState.p1Result - 10;  
+        let output = gameState.p1Result - Math.max(gameState.p1Money, 10) ;
         outputData.push(output);
     });
     //console.log(inputData);
@@ -113,26 +114,42 @@ const preprocess = (gameStates) => {
 
 async function trainModel(games, numEpochs, model = null) {
     const [xs, ys, inputLenght] = preprocess(games);
-    
+
     model = tf.sequential();
-    model.add(tf.layers.dense({ units: 32, activation: 'relu', inputShape: [inputLenght], kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }), biasRegularizer: tf.regularizers.l2({ l2: 0.01 })  }));
+    model.add(tf.layers.dense({ units: 32, activation: 'relu', inputShape: [inputLenght], kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }), biasRegularizer: tf.regularizers.l2({ l2: 0.01 }) }));
     model.add(tf.layers.dense({ units: 64, activation: 'relu', kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }), biasRegularizer: tf.regularizers.l2({ l2: 0.01 }) }));
     model.add(tf.layers.dense({ units: 64, activation: 'relu', kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }), biasRegularizer: tf.regularizers.l2({ l2: 0.01 }) }));
     model.add(tf.layers.dense({ units: 64, activation: 'relu', kernelRegularizer: tf.regularizers.l2({ l2: 0.01 }), biasRegularizer: tf.regularizers.l2({ l2: 0.01 }) }));
     model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
     model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
-    await model.fit(xs, ys, {
-        epochs: numEpochs,
-        validationSplit: 0.3,
-        callbacks: {
-            onEpochEnd: (epoch, logs) => {
-              const logInterval = Math.floor(numEpochs / 6); // Calculate the interval for logging
-              if (epoch === 0 || epoch === numEpochs - 1 || epoch % logInterval === 0) {
-                console.log(`Epoch ${epoch}: loss = ${logs.loss}`);
-              }
-            }
-          }
-    });
+
+    const patience = 20;
+    const min_delta = 0.001;
+    let bestValLoss = Number.MAX_VALUE;
+    let count = 0;
+    const logInterval = Math.floor(numEpochs / 6);
+
+    for (let epoch = 0; epoch < numEpochs; epoch++) {
+        const history = await model.fit(xs, ys, {
+            epochs: 1,
+            validationSplit: 0.2,
+        });
+        const valLoss = history.history.val_loss[0];
+        if (valLoss < bestValLoss - min_delta) {
+            bestValLoss = valLoss;
+            count = 0;
+        } else {
+            count++;
+        }
+        if (epoch === 0 || epoch === numEpochs - 1 || epoch % logInterval === 0) {
+            console.log(`Epoch ${epoch}: loss = ${history.history.loss}, val_loss = ${valLoss}`);
+        }
+        if (count >= patience) {
+            console.log('Early stopping');
+            break;
+        }
+    }
+  
     return model;
 }
 
@@ -148,21 +165,22 @@ async function aiPlayer(model, gameState, player) {
         let input = [
             currentHand,
             possibleBet,
-            
+            maxBet,
+            gameState.round
         ];
 
         let prediction = model.predict(tf.tensor2d([input]));
         let winProbability = prediction.dataSync()[0];
-       console.log("Bet:"+possibleBet +"  p1Hand:" + gameState.p1Hand,"  WinPredic:" + winProbability)
+       // console.log("Bet:" + possibleBet + "  p1Hand:" + gameState.p1Hand, "  WinPredic:" + winProbability)
 
         if (winProbability > bestBetProbability) {
             bestBet = possibleBet;
             bestBetProbability = winProbability;
         }
     }
-     console.log("CurrentHand  " + currentHand )
-     console.log("BETTING  " + bestBet);
-     console.log("bestBetProbability  " + bestBetProbability);
+    console.log("CurrentHand  " + currentHand)
+    console.log("BETTING  " + bestBet);
+    console.log("bestBetProbability  " + bestBetProbability);
     return bestBet
 }
 
@@ -170,9 +188,9 @@ async function aiPlayer(model, gameState, player) {
 async function fixedPlayer(gameState, player) {
     const hand = player == 1 ? gameState.p1Hand : gameState.p2Hand;
     let bet = 0;
-    if(hand > 5) {
+    if (hand > 5) {
         bet = hand
-    } 
+    }
     return bet
 }
 
@@ -188,28 +206,35 @@ function getRandom1to10() {
 async function runGame(player1Function, player2Function) {
     let p1Money = 10;
     let p2Money = 10;
-    const p1Hand = getRandom1to10();
-    const p2Hand = getRandom1to10();
-    let gameState = {
-        p1Money,
-        p2Money,
-        p1Hand,
-        p2Hand,
+    let gameStates = [];
+
+    for (let round = 1; round <= 2; round++) {
+        const p1Hand = getRandom1to10();
+        const p2Hand = getRandom1to10();
+        let newGameState = {
+            round,
+            p1Money: round == 1 ? p1Money : gameStates[0].p1Result,
+            p2Money: round == 1 ? p1Money : gameStates[0].p2Result,
+            p1Hand,
+            p2Hand,
+        }
+        let p1Bet = await player1Function(newGameState, 1);
+        p1Bet = Math.min(p1Bet, p1Money);
+        let p2Bet = await player2Function(newGameState, 2);
+        p2Bet = Math.min(p2Bet, p2Money);
+        newGameState = { ...newGameState, p1Bet, p2Bet }
+
+        if (p1Hand > p2Hand) {
+            p1Money += p1Bet
+            p2Money -= p2Bet
+        } else if (p1Hand < p2Hand) {
+            p2Money += p2Bet
+            p1Money -= p1Bet
+        }
+        gameStates.push({ ...newGameState, p1Result: p1Money, p2Result: p2Money })
     }
-    let p1Bet = await player1Function(gameState, 1);
-    p1Bet = Math.min(p1Bet, p1Money);
-    let p2Bet = await player2Function(gameState, 2);
-    p2Bet = Math.min(p2Bet, p2Money);
-    gameState = { ...gameState, p1Bet, p2Bet }
-    if (p1Hand > p2Hand) {
-        p1Money += p1Bet
-        p2Money -= p2Bet
-    } else  if(p1Hand < p2Hand) {
-        p2Money += p2Bet
-        p1Money -= p1Bet
-    }
-    gameState = { ...gameState, p1Result: p1Money, p2Result: p2Money }
-    return gameState
+    
+    return gameStates
 }
 
 async function runGames(player1Function, player2Function, numGames) {
@@ -220,20 +245,21 @@ async function runGames(player1Function, player2Function, numGames) {
     let p2Earnings = 0;
     let ties = 0;
     for (let i = 0; i < numGames; i++) {
-        const gameState = await runGame(player1Function, player2Function)
-        if (gameState.p1Result > gameState.p2Result) {
+        const gameStateMultipleRounds = await runGame(player1Function, player2Function)
+        const finalGameState = gameStateMultipleRounds[gameStateMultipleRounds.length-1];
+        if (finalGameState.p1Result > finalGameState.p2Result) {
             p1Wins++
-        } else if (gameState.p1Result < gameState.p2Result) {
+        } else if (finalGameState.p1Result < finalGameState.p2Result) {
             p2Wins++
         } else {
             ties++;
         }
-        p1Earnings += gameState.p1Result;
-        p2Earnings += gameState.p2Result;
-        gameStates.push(gameState)
+        p1Earnings += finalGameState.p1Result;
+        p2Earnings += finalGameState.p2Result;
+        gameStates.push(...gameStateMultipleRounds)
     }
-    console.log("Player 1 Earnings " + p1Earnings + "     Avg:" + Math.round(p1Earnings/numGames) + "   Wins:" +p1Wins + "   Ties:" +ties)
-    console.log("Player 2 Earnings " + p2Earnings + "     Avg:" + Math.round(p2Earnings/numGames) + "   Wins:" +p2Wins)
+    console.log("Player 1 Earnings " + p1Earnings + "     Avg:" + Math.round(p1Earnings / numGames) + "   Wins:" + p1Wins + "   Ties:" + ties)
+    console.log("Player 2 Earnings " + p2Earnings + "     Avg:" + Math.round(p2Earnings / numGames) + "   Wins:" + p2Wins)
     console.log(gameStates);
     return gameStates;
 }
